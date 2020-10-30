82c061fabc21b6c90f3ab9372940d12f0dc9393c
SterlingDuke <sterlingcduke@gmail.com>
2020-10-30 17:56:30 -0500

    var specialchar

--- Develop/script.js
+++ Develop/script.js
@@ -4,3 +4,7 @@ var upperCase = false;
 var numeric = false;
 var specialCharacters = false;
 var choiceArr = []; 
+
+var specialCharArr = ['\u0021', '\u0023', '\u0024', '\u0025', '\u0026', '\u002A', '\u002B', '\u003C', '\u003F', '\u003E', '\u003D', '\u0040', '\u005E', '\u007B', '\u007D'];
+
+}
